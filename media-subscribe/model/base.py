ad47eda82a35960462af7f5948b46c63f4291656
klaxonz <klaxon5296@gmail.com>
2024-06-30 15:20:25 +0800

    feat(model): add dynamic table name generation for base model
    - Implement a `make_table_name` function to dynamically generate table names
      based on model class names.
    - Modify the `BaseModel` class to use the `make_table_name` function as the
      `table_function` in the `Meta` class to ensure table names follow a consistent
      and dynamic naming convention.
    
    This change improves the flexibility and maintainability of the model layer by
    automatically generating table names based on model class names, avoiding
    hardcoded table names and simplifying the process of adding new models.

--- media-subscribe/model/base.py
+++ media-subscribe/model/base.py
@@ -3,6 +3,12 @@ from peewee import Model
 from common.database import DbInstanceHolder
 
 
+def make_table_name(model_class):
+    model_name = model_class.__name__
+    return ''.join(['_' + c.lower() if c.isupper() else c for c in model_name]).lstrip('_')
+
+
 class BaseModel(Model):
     class Meta:
+        table_function = make_table_name
         database = DbInstanceHolder.get_instance()
