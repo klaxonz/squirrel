02455d9d03eb62e20fd18ea47707f6908d985827
klaxonz <klaxon5296@gmail.com>
2024-09-14 23:40:05 +0800

    refactor(SearchBar): update input placeholder and button styles

--- media-subscribe-front/src/components/SearchBar.vue
+++ media-subscribe-front/src/components/SearchBar.vue
@@ -1,25 +1,27 @@
 <template>
   <div :class="['search-bar', { 'hidden': isScrollingUp }]" ref="searchBar">
     <div class="flex items-center w-11/12 mx-auto relative ">
-      <input
-        v-model="searchQuery"
-        @keyup.enter="handleSearch"
-        type="text"
-        placeholder="搜索视频..."
-        class="search-input flex-grow h-7 px-3 pr-8 text-xs border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200"
-      >
-      <button
-        v-if="searchQuery"
-        @click="clearSearch"
-        class="clear-button absolute right-16 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 focus:outline-none"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/>
-        </svg>
-      </button>
+      <div class="relative flex-grow">
+        <input
+          v-model="searchQuery"
+          @keyup.enter="handleSearch"
+          type="text"
+          placeholder="搜索视频..."
+          class="search-input w-full h-7 px-3 pr-3 text-xs border border-gray-300 rounded-l-full focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200"
+        >
+        <button
+          v-if="searchQuery"
+          @click="clearSearch"
+          class="clear-button absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 focus:outline-none"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/>
+          </svg>
+        </button>
+      </div>
       <button
         @click="handleSearch"
-        class="search-button h-7 px-3 text-xs font-medium bg-blue-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 focus:ring-offset-2 transition duration-200"
+        class="search-button h-7 px-3 text-xs font-medium bg-blue-500 text-white rounded-r-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 focus:ring-offset-2 transition duration-200"
       >
         搜索
       </button>
@@ -67,10 +69,6 @@ const emit = defineEmits(['search']);
 .search-input {
   border-right: none;
   border-radius: 9999px 0 0 9999px;
-}
-
-.clear-button {
-  right: 2rem;
 }
 
 .search-button {
