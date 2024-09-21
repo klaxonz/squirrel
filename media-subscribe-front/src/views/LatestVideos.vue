2397726f967a7b92911fff51ad183135d0f2c1b4
klaxonz <klaxon5296@gmail.com>
2024-09-21 11:42:04 +0800

    refactor(LatestVideos): remove toast message styles from component

--- media-subscribe-front/src/views/LatestVideos.vue
+++ media-subscribe-front/src/views/LatestVideos.vue
@@ -395,18 +395,5 @@ provide('videoOperations', {
 
 <style scoped>
 /* Add this to your component's styles */
-.toast-message {
-  position: fixed;
-  bottom: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: rgba(0, 0, 0, 0.8);
-  color: white;
-  padding: 10px 20px;
-  border-radius: 5px;
-  z-index: 9999;
-  font-size: 16px;
-  max-width: 80%;
-  text-align: center;
-}
+
 </style>
