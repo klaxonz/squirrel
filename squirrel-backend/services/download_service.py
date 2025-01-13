ab83087d93c087dee4c9c1aa13bad49767cf7f19
klaxonz <klaxon5296@gmail.com>
2025-01-13 22:47:03 +0800

    feat(download_service): add subscription enable check before video download
    - Add function to check if subscription is enabled by subscription_id
    - Implement the enable check before starting video download
    - Return early if subscription is not enabled

--- squirrel-backend/services/download_service.py
+++ squirrel-backend/services/download_service.py
@@ -6,7 +6,7 @@ from common import constants
 from consumer import extract_task
 from core.cache import RedisClient
 from dto.video_dto import VideoExtractDto
-from services import video_service, message_service
+from services import video_service, message_service, subscription_service
 
 logger = logging.getLogger()
 client = RedisClient.get_instance().client
@@ -15,9 +15,7 @@ client = RedisClient.get_instance().client
 def __check_video_exists(url: str) -> bool:
     if video_service.get_video_by_url(url):
         return True
-
     extract_timestamp = task_cache.get_extract_cache(url)
-
     if not extract_timestamp:
         return False
     time_elapsed = datetime.datetime.now().timestamp() - float(extract_timestamp)
@@ -29,10 +27,17 @@ def __check_video_exists(url: str) -> bool:
     return is_extracting
 
 
+def __check_subscription_enable(subscription_id: int):
+    subscription = subscription_service.get_subscription_by_id(subscription_id)
+    return subscription.is_enable == 1
+
+
 def start(params: VideoExtractDto):
     if params.only_extract:
         if __check_video_exists(params.url):
             return
+    if __check_subscription_enable(params.subscription_id) is False:
+        return
     task_cache.set_extract_cache(params.url, constants.VIDEO_EXTRACT_FIELD_NAME)
     content = params.model_dump()
     message = message_service.create_message(content)
