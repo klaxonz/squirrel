0bedfef95f390949aa3cb66f4d733fa211962042
klaxonz <klaxon5296@gmail.com>
2025-02-11 23:26:19 +0800

    refactor(subscribe_task): optimize subscription logic and remove redundancy

--- squirrel-backend/consumer/subscribe_task.py
+++ squirrel-backend/consumer/subscribe_task.py
@@ -4,6 +4,7 @@ import dramatiq
 from common import constants
 from core.database import get_session
 from models.message import Message
+from models.subscription import Subscription
 from services import subscription_service
 from subscribe.factory import SubscriptionFactory
 
@@ -19,22 +20,19 @@ def process_subscribe_message(message):
         user_id = json.loads(message_obj.body)['user_id']
         subscribe_channel = SubscriptionFactory.create_subscription(url)
         subscribe_info = subscribe_channel.get_subscribe_info()
-        videos = subscribe_channel.get_subscribe_videos(extract_all=True)
 
-        with get_session() as session:
-            subscription = subscription_service.get_subscription_by_url_and_name(url, subscribe_info.name)
-            if subscription and not subscription.is_deleted:
-                logger.info(f"Already subscribed to this channel: {subscription.name}")
-                return
-            if subscription and subscription.is_deleted:
-                session.merge(subscription)
-                subscription.total_videos = len(videos)
-                subscription.is_deleted = False
-            else:
-                subscription = subscription_service.create_subscription(user_id, subscribe_info)
-                session.merge(subscription)
-                subscription.total_videos = len(videos)
-            session.commit()
-            logger.info(f"Successfully subscribed: {subscription.name}")
+        subscription = subscription_service.get_subscription_by_url_and_name(url, subscribe_info.name)
+        if subscription and not subscription.is_deleted:
+            logger.info(f"Already subscribed to this channel: {subscription.name}")
+            return
+        if subscription and subscription.is_deleted:
+            with get_session() as session:
+                session.query(Subscription).filter(Subscription.id == subscription.id).update({
+                    Subscription.is_deleted: 0
+                })
+                session.commit()
+        else:
+            subscription_service.create_subscription(user_id, subscribe_info)
+        logger.info(f"Successfully subscribed: {subscription.name}")
     except Exception as e:
         logger.error(f"Error occurred while adding subscription: {e}", exc_info=True)
