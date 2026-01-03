3c2252d19c9973316e57a0847f089a23eb206ee1
klaxonz <klaxon5296@gmail.com>
2026-01-03 18:16:48 +0800

    refactor(scheduler, worker): restructure imports and add manager modules for scheduler and worker functionality

--- squirrel-backend/processes/worker_process.py
+++ squirrel-backend/processes/worker_process.py
@@ -2,7 +2,7 @@ import logging
 import time
 
 from common.constants import SYS_ENABLE_WORKER
-from managers.worker_manager import worker_start, worker_stop
+from processes.managers.worker_manager import worker_start, worker_stop
 from processes.service_runtime import bootstrap_runtime, create_shutdown_event
 from services import system_config_service
 
