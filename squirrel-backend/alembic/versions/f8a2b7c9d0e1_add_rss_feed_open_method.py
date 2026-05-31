481cf40bce6b1d96a4be628610a541d7ff49121b
klaxonz <klaxon5296@gmail.com>
2026-05-31 11:11:28 +0800

    chore(db): add rss_feed.open_method column migration

---
+++ squirrel-backend/alembic/versions/f8a2b7c9d0e1_add_rss_feed_open_method.py
@@ -0,0 +1,24 @@
+"""add rss_feed.open_method column
+
+Revision ID: f8a2b7c9d0e1
+Revises: e7f9b0a3c2d1
+Create Date: 2026-05-31 11:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f8a2b7c9d0e1'
+down_revision = 'e7f9b0a3c2d1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('rss_feed', sa.Column('open_method', sa.VARCHAR(32), nullable=True))
+
+
+def downgrade():
+    op.drop_column('rss_feed', 'open_method')
